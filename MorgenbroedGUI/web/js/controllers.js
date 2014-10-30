8119fe2557cd9eca2f29df405387093df2611981
hvagit <henrik1969@gmail.com>
2014-10-30 22:09:11 +0100

    Tilrettet header.

--- MorgenbroedGUI/web/js/controllers.js
+++ MorgenbroedGUI/web/js/controllers.js
@@ -8,7 +8,30 @@ morgenBroedControllers.controller('OrdreCtrl', function($scope,
 });
 
 morgenBroedControllers.controller('AdminCtrl', function($scope, $routeParams,
-		$location, $http, dataService, opdateringsData) {
+		$location, $http) {
+                    
+     $scope.submitBatch = function() 
+      {
+        var promise = $http({
+        method: 'POST',
+        url: 'http://localhost:8080/MorgenbroedBatch/JobSubmitterServlet',
+        headers: {'Content-Type': 'application/json'},
+        data:  $scope.input
+        });
+                
+         promise.success(function (data) 
+        {
+            alert("Succes");
+            $scope.status=data;
+        });
+
+         promise.error(function (data) 
+        {
+            alert("Fejl");
+            $scope.status=data;
+        });
+     };
+                    
 
 });
 
@@ -17,20 +40,20 @@ morgenBroedControllers.controller('LoginCtrl', function($scope,
                     
                     
                 $scope.login = function() {
-                if($scope.brugerModel =='admin')
-                {
-                    if($scope.passwordModel.length>0)
+                    if($scope.brugerModel ==='admin')
                     {
-        		$location.path('/admin');
+                        if($scope.passwordModel !== null && $scope.passwordModel.length>0)
+                        {
+                            $location.path('/admin');
+                        }
                     }
-                }
-                if($scope.brugerModel =='jhl' || $scope.brugerModel =='jmn' || $scope.brugerModel =='hve')
-                {
-                    if($scope.passwordModel.length>0)
+                    if($scope.brugerModel ==='jhl' || $scope.brugerModel ==='jmn' || $scope.brugerModel ==='hve')
                     {
-        		$location.path('/ordre');
+                        if($scope.passwordModel !== null && $scope.passwordModel.length>0)
+                        {
+                            $location.path('/ordre');
+                        }
                     }
-                }
-	}
+                };
 
 });
