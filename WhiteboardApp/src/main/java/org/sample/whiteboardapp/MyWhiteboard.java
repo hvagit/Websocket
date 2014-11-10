a1d931b1132f259e3cb41e84ea9845e5578a2f98
hvagit <henrik1969@gmail.com>
2014-11-10 23:26:45 +0100

    Tilrettet logning.

---
+++ WhiteboardApp/src/main/java/org/sample/whiteboardapp/MyWhiteboard.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sample.whiteboardapp;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.OnClose;
+import javax.websocket.OnOpen;
+import javax.websocket.OnMessage;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
+
+/**
+ *
+ * @author Henrik
+ */
+@ServerEndpoint(value="/whiteboardendpoint", encoders = {FigureEncoder.class}, decoders = {FigureDecoder.class})
+public class MyWhiteboard {
+
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+    
+//    @OnMessage
+//    public void broadcastFigure(Figure figure, Session session) throws IOException, EncodeException {
+//        System.out.println("broadcastFigure: " + figure);
+//        for (Session peer : peers) {
+//            if (!peer.equals(session)) {
+//                peer.getBasicRemote().sendObject(figure);
+//            }
+//        }
+//    }
+    
+    @OnMessage
+    public void broadcastSnapshot(ByteBuffer data, Session session) throws IOException {
+    System.out.println("broadcastBinary: " + data);
+    for (Session peer : peers) {
+        if (!peer.equals(session)) {
+            peer.getBasicRemote().sendBinary(data);
+        }
+        }
+    }
+
+    @OnOpen
+    public void onOpen (Session peer) {
+        peers.add(peer);
+    }
+
+    @OnClose
+    public void onClose (Session peer) {
+        peers.remove(peer);
+    }
+}
